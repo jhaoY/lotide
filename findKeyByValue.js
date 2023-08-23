ca979dbadabed31c65027a5ab84ee71632d3b344
Justin Hao <justinhao@live.com>
2023-08-23 18:35:46 -0400

    Removed test code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -22,8 +22,6 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
-findKeyByValue(bestTVShowsByGenre, "The Wire");
-
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), undefined);
