ef676e6754b48d2ae375a27e5f2600382a585e46
Justin Hao <justinhao@live.com>
2023-08-23 18:05:24 -0400

    Adding letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (results[char]) {
+      results[char].push(i);
+    } else {
+      results[char] = [i];
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
