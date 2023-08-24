7475b80b81834c6b7acc1d7846b53d3d5686ab6b
Justin Hao <justinhao@live.com>
2023-08-24 17:33:34 -0400

    Adding map.js

---
+++ map.js
@@ -0,0 +1,37 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word + 1);
+const results3 = map(words, word => word.length);
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(results2, ["ground1", "control1", "to1", "major1", "tom1"]);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
