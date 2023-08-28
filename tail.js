0b31d8fb160b74bf5b07c1be95cf395ae2ed3c5f
Justin Hao <justinhao@live.com>
2023-08-28 15:21:51 -0400

    Moved test code to tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   if (arr.length > 0) {
     let tailValue = arr.slice(1);
@@ -15,21 +7,23 @@ const tail = function(arr) {
   }
 };
 
-// Test Case: Verify the original array has not been modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
 
-// Test Case: Verify elements of modified array are correct
-let result = tail(words);
-assertEqual(result[0], "Lighthouse");
+// // Test Case: Verify the original array has not been modified
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
 
-// Test Case: Verify array with one element returns an empty array
-const oneWordArr = ["Hi"];
-result = tail(oneWordArr);
-assertEqual(result.length, 0);
+// // Test Case: Verify elements of modified array are correct
+// let result = tail(words);
+// assertEqual(result[0], "Lighthouse");
 
-//Test Case: Verify empty array returns an empty array
-const emptyArr = [];
-result = tail(emptyArr);
-assertEqual(result.length, 0);
+// // Test Case: Verify array with one element returns an empty array
+// const oneWordArr = ["Hi"];
+// result = tail(oneWordArr);
+// assertEqual(result.length, 0);
+
+// //Test Case: Verify empty array returns an empty array
+// const emptyArr = [];
+// result = tail(emptyArr);
+// assertEqual(result.length, 0);
