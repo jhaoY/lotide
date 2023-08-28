8aae9db5b28bf6557a7ecdf18a453bc564c9bf8e
Justin Hao <justinhao@live.com>
2023-08-28 16:03:21 -0400

    Refactored to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,25 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Case: Verify the original array has not been modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-// Test Case: Verify elements of modified array are correct
-let result = tail(words);
-assertEqual(result[0], "Lighthouse");
-
-// Test Case: Verify array with one element returns an empty array
-const oneWordArr = ["Hi"];
-result = tail(oneWordArr);
-assertEqual(result.length, 0);
-
-//Test Case: Verify empty array returns an empty array
-const emptyArr = [];
-result = tail(emptyArr);
-assertEqual(result.length, 0);
+describe("#tail", () => {
+  it("returns identical array length to ensure original array has not been modified", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  })
+  it("returns element of modified array and verifies it is correct", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"]
+    result = tail(words)
+    assert.strictEqual(result[0], "Lighthouse");
+  })
+  it("returns empty array when array of one element is given", () => {
+    const oneWordArr = ["Hi"];
+    result = tail(oneWordArr);
+    assert.strictEqual(result.length, 0);
+  })
+  it("returns empty array when given an empty array", () => {
+    const emptyArr = [];
+    result = tail(emptyArr);
+    assert.strictEqual(result.length, 0);
+  })
+});
