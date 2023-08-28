11d0662f68fde1d86a385d34c02834615ea42d96
Justin Hao <justinhao@live.com>
2023-08-28 15:22:31 -0400

    Removed commented test code

--- tail.js
+++ tail.js
@@ -8,22 +8,3 @@ const tail = function(arr) {
 };
 
 module.exports = tail;
-
-// // Test Case: Verify the original array has not been modified
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
-// // Test Case: Verify elements of modified array are correct
-// let result = tail(words);
-// assertEqual(result[0], "Lighthouse");
-
-// // Test Case: Verify array with one element returns an empty array
-// const oneWordArr = ["Hi"];
-// result = tail(oneWordArr);
-// assertEqual(result.length, 0);
-
-// //Test Case: Verify empty array returns an empty array
-// const emptyArr = [];
-// result = tail(emptyArr);
-// assertEqual(result.length, 0);
