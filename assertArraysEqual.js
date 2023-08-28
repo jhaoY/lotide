33d5fc2f870c672ca4e5bd40c4d4c9eafbb28477
Justin Hao <justinhao@live.com>
2023-08-28 15:32:33 -0400

    Refactored to remove unnecessary eqArrays function and require it instead

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require ('./eqArrays')
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -17,3 +7,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
